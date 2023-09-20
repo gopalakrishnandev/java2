e72a81f212906825ad0e7c3ab05d821445c4c1e9
gopalakrishnan <semsampathkumar@gmail.com>
2023-09-20 23:54:54 +0530

    array vs array list part1

---
+++ src/ArrayList/Main.java
@@ -0,0 +1,28 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+record GeoceryItem(String name, String type, int count){
+        public  GeoceryItem(String name, String type){
+            this(name, type, 12);
+        }
+}
+public class Main {
+
+
+        //Array
+    public static void main(String[] args) {
+        GeoceryItem[] gerceryArray = new GeoceryItem[3];
+        gerceryArray[0] = new GeoceryItem("sachin", "pp");
+        gerceryArray[1] = new GeoceryItem("Dhoni", "yy ");
+        gerceryArray[2] = new GeoceryItem("Virat", "kk");
+
+        System.out.println(Arrays.toString(gerceryArray));
+
+        //Array list
+        ArrayList<GeoceryItem> arrList = new ArrayList<>();
+        arrList.add(new GeoceryItem("Sam", "human"));
+        System.out.println(arrList);
+    }
+}
---
+++ src/Generics/Main.java
@@ -0,0 +1,10 @@
+package Generics;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Yes");
+
+        record CricketPlayers(String name, int age, String role ){};
+
+    }
+}
